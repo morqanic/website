d881d160f9a642a24cfa854739db90c5a19a7ead
Morgan Swaak <m.swaak@student.unsw.edu.au>
2026-09-21 23:24:51 +1000

    added comments explaining mistakes i made and why i had two timeouts

---
+++ index.js
@@ -0,0 +1,44 @@
+let star = (x, y) => {
+  const newStar = document.createElement("div");
+  newStar.textContent = "\u2726"
+  newStar.style.position = "absolute";
+  newStar.style.left = x + window.scrollX + "px";
+  newStar.style.top = y + window.scrollY + "px";
+
+  newStar.style.color = "yellow";
+  newStar.style.fontSize = "32px";
+  // this was very helpfull, before I had user-select: none which meant the divs would often get in the way
+  newStar.style.pointerEvents = "none";
+  newStar.style.transform = "translate(-50%, -50%)";
+  document.body.appendChild(newStar);
+  return newStar;
+}
+
+lastStar = {
+  "x": 0,
+  "y": 0
+};
+
+mouse = {
+  "x": 0,
+  "y": 0
+};
+
+document.addEventListener('mousemove', (event) => {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+
+  const x = event.clientX; 
+  const y = event.clientY;
+  if (Math.hypot(lastStar.x - x, lastStar.y - y) > 8) {
+    lastStar.x = x
+    lastStar.y = y
+    // make sure stars dont appear at cursor and if moving slowly no stars
+    setTimeout(() => {
+      if (Math.hypot(mouse.x - x, mouse.y - y) > 4) {
+        const newStar = star(x,y);
+        setTimeout(() => {newStar.remove()}, 100)
+      }
+    }, 25)
+  }
+});
